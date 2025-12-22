ff5e363ebcc9c7d7a5cbead857a3140d79672607
AGVelez <65133417+agvelez07@users.noreply.github.com>
2025-12-22 15:10:22 +0000

    Refactor character creation function and constants

--- character.c
+++ character.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define rad  0;
-#define slice 32;
-#define stacl 24;
+#define RAD  0.5;
+#define SLICE 32;
+#define STACK 24;
 
-void createCharacter(int x) {
-  glutSolidSphere(	rad , slices , stacks );
+void createCharacter() {
   glColour3f(255,255,0);
+  glutSolidSphere(RAD , SLICE ,STACK );
 }
