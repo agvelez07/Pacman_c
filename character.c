d3ee2074fb3b810a5e70c12db45a98b41ebe464f
AGVelez <65133417+agvelez07@users.noreply.github.com>
2025-12-23 16:23:29 +0000

    Refactor character functions and fix memory allocation

--- character.c
+++ character.c
@@ -12,33 +12,59 @@ int r;
 int c;
 }Pacman;
 
-void getMapInd(char** m, HousePos* housePosArr){
-  if(housePosARR && m){
-    int count = 0;
-    for(int i = 0; i < xTable(); i++){
-      for(int j = 0; j < yTable(); j++){
+int getMapInd(char** m, HousePos* housePosArr){
+ int count = 0;
+ if(housePosArr && m){
+    for(int i = 0; i < xTabSize(); i++){
+      for(int j = 0; j < yTabSize(); j++){
         if(m[i][j] == 1){
-            housePosArr[count].r = i;
-            housePosArr[count].c = j;
+            housePosArr[count].r = j;
+            housePosArr[count].c = i;
             count++;
           }
         }
       }
     }
   }
+ return count;
 }
 
-HousePos createhousePos(){
-  int max = int max = xTabSize() * yTabSize();
-  HousePos* housePost = malloc(SizeOf(HousePos * max);
-  return housePos;  
+HousePos* createHousePos(int* outMax)
+{
+    int max = xTabSize() * yTabSize();
+    if (outMax) *outMax = max;
+    return malloc(sizeof(HousePos) * max);
 }
 
+void polygon(int a, int b, int c, int d)
+{
+	 glBegin(GL_POLYGON); 
+		glNormal3fv(normals[a]);
+		glVertex3fv(vertices[a]); 
+		glNormal3fv(normals[b]);
+		glVertex3fv(vertices[b]); 
+		glNormal3fv(normals[c]);
+		glVertex3fv(vertices[c]); 
+		glNormal3fv(normals[d]);
+		glVertex3fv(vertices[d]);
+	glEnd();
+}
 
+void  createPacman(void)
+{
+	glColor3f(1.0f, 1.0f, 0.0f);
+	polygon(0, 3, 2, 1); 
+	polygon(2, 3, 7, 6); 
+ polygon(0, 4, 7, 3); 
+ polygon(1, 2, 6, 5); 
+	polygon(4, 5, 6, 7); 
+	polygon(0, 1, 5, 4);
+}
 
 int createCharacter(char** m) {
-  HousePos* housePosArr = createhousePos();
-  getMapInd(m, housePosArr);
-  createPacman();
+ int max = 0;
+ HousePos* housePosArr = createHousePos(&max);
 
+ getMapInd(m, housePosArr);
+ createPacman();
 }
