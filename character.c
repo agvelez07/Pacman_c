853bcb79be873d27e2058f914a2bc80b93088830
AGVelez <65133417+agvelez07@users.noreply.github.com>
2025-12-23 15:00:59 +0000

    Update character.c
    Working from office. No VS.

--- character.c
+++ character.c
@@ -6,7 +6,32 @@
 #define SLICE 32;
 #define STACK 24;
 
-void createCharacter() {
-  glColour3f(255,255,0);
-  glutSolidSphere(RAD , SLICE ,STACK );
+struct housePos{
+int r;
+int c;
+}HousePos;
+
+int getMapInd(char** m, int index[][2]){
+  int r = 0;
+  for(int i = 0; i < xTable(); i++){
+    for(int j = 0; j < yTable(); j++){
+      if(m[i][j] == 1){
+        index[r][0] = m[i];
+        index[r][1] = m[j];
+        r++;
+      }
+    }
+  }
+  return index;
+}
+
+HousePos createhousePos(){
+  HousePos housePost = malloc(HousePos)SizeOf(housePos);
+  return housePos;  
+}
+
+int createCharacter(char** m) {
+  createhousePos();
+  getMapInd(m, index);
+
 }
