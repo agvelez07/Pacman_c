70333bffe097febc4030d2e41dcf2d856db17f4a
AGVelez <65133417+agvelez07@users.noreply.github.com>
2025-12-23 15:14:55 +0000

    Refactor getMapInd function and housePos structure

--- character.c
+++ character.c
@@ -1,28 +1,27 @@
 #include "pm-maps.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-#define RAD  0.5;
-#define SLICE 32;
-#define STACK 24;
-
-struct housePos{
+ 
+type struct{
 int r;
 int c;
 }HousePos;
 
-int getMapInd(char** m, int index[][2]){
-  int r = 0;
-  for(int i = 0; i < xTable(); i++){
-    for(int j = 0; j < yTable(); j++){
-      if(m[i][j] == 1){
-        index[r][0] = m[i];
-        index[r][1] = m[j];
-        r++;
+void getMapInd(char** m){
+  if(housePos){
+  {
+    int count = 0;
+    for(int i = 0; i < xTable(); i++){
+      for(int j = 0; j < yTable(); j++){
+        if(m[i][j] == 1){
+            housePost[count].r = i;
+            housePost[count].c = j;
+            count++;
+          }
+        }
       }
     }
   }
-  return index;
 }
 
 HousePos createhousePos(){
@@ -33,6 +32,6 @@ HousePos createhousePos(){
 
 int createCharacter(char** m) {
   createhousePos();
-  getMapInd(m, index);
+  getMapInd(m);
 
 }
